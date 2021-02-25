4d5e47bb51c2e7732389bfeadaa7bc478e316783
double-em <meldgaardmike@gmail.com>
2021-02-25 13:10:05 +0100

    Initial

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+using WebApp.Service;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(BookingModel booking)
+        {
+            switch (booking.ActionType)
+            {
+                case BookingType.Book:
+                    Publisher.PublishMessage(Constants.Channels.Tour.Booked, booking.ToString());
+                    break;
+                
+                case BookingType.Cancel:
+                    Publisher.PublishMessage(Constants.Channels.Tour.Cancelled, booking.ToString());
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ WebApp/Models/BookingModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApp.Models
+{
+    public enum BookingType
+    {
+        Book,
+        Cancel
+    }
+    public class BookingModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Tour { get; set; }
+        
+        public BookingType ActionType { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}, {Tour}, {ActionType.ToString().ToLower()}";
+        }
+    }
+}
